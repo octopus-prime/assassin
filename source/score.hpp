95b2cf100ccbffc577be695fc139937db62ed4e4
mike <mike@workstation>
2016-08-06 16:22:02 +0200

    Added score

---
+++ source/score.hpp
@@ -0,0 +1,22 @@
+/*
+ * score.hpp
+ *
+ *  Created on: 06.08.2016
+ *      Author: mike
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace chess {
+
+typedef std::int16_t score_t;
+
+//enum color : color_t
+//{
+//	white = +1,
+//	black = -1
+//};
+
+}
