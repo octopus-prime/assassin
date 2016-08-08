3cdd343ce529e94478fdbfc35cee25d7e324742b
mike <mike@workstation>
2016-08-08 20:52:06 +0200

    Added tests for move generator.

--- test/test_move_generator.cpp
+++ test/test_move_generator.cpp
@@ -29,13 +29,15 @@ BOOST_AUTO_TEST_SUITE(test_move_generator)
 const std::initializer_list<node_t> nodes
 {
 	node_t(node_t::INITIAL_POSITION),
-	node_t("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 1 1")
+	node_t("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 1 1"),
+	node_t("r3k2r/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 1 1")
 };
 
 BOOST_AUTO_TEST_SUITE(test_active)
 
 const std::initializer_list<std::initializer_list<move_t>> movess
 {
+	{},
 	{},
 	{}
 };
@@ -53,7 +55,8 @@ BOOST_AUTO_TEST_SUITE(test_all)
 const std::initializer_list<std::initializer_list<move_t>> movess
 {
 	{{b1, a3}, {b1, c3}, {g1, f3}, {g1, h3}},
-	{{b8, a6}, {b8, c6}, {g8, f6}, {g8, h6}}
+	{{b8, a6}, {b8, c6}, {g8, f6}, {g8, h6}},
+	{{e8, d8}, {e8, f8}, {e8, g8}, {e8, c8}, {a8, b8}, {a8, c8}, {a8, d8}, {h8, f8}, {h8, g8}}
 };
 
 BOOST_DATA_TEST_CASE(test, nodes ^ movess, node, moves)
