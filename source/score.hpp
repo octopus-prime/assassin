f1773d29f934cd78c08a7bb4176993cd5a78acaa
mike <mike@workstation>
2016-08-14 15:18:58 +0200

    Score constants.

--- source/score.hpp
+++ source/score.hpp
@@ -16,17 +16,12 @@ typedef std::int16_t score_t;
 static constexpr std::array<score_t, 13> score_of
 {
 	0,
-	+30000, +900, +500, +333, +325, +100,
-	+30000, +900, +500, +333, +325, +100
-};
+	0, +900, +500, +333, +325, +100,
+	0, +900, +500, +333, +325, +100
 
-static constexpr std::array<score_t, 5> scoring
-{
-	0,
-	score_of[N] - score_of[P],
-	score_of[B] - score_of[P],
-	score_of[R] - score_of[P],
-	score_of[Q] - score_of[P]
+// todo: king scores should not be zero, to sort them last in capture lists...
+//	+30000, +900, +500, +333, +325, +100,
+//	+30000, +900, +500, +333, +325, +100
 };
 
 }
