2508af5c0e0afef34507b3a525c2d3d6569cb201
octopus-prime <mike.gresens@gmail.com>
2017-10-01 12:12:24 +0200

    Added tests for move

---
+++ test/test_move.cpp
@@ -0,0 +1,53 @@
+/*
+ * test_move.cpp
+ *
+ *  Created on: 01.10.2017
+ *      Author: mike
+ */
+
+#include "move.hpp"
+#include "io.hpp"
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+
+namespace chess {
+
+static_assert(sizeof(move_t) == 4, "move_t size");
+
+BOOST_AUTO_TEST_SUITE(test_move)
+
+constexpr std::initializer_list<move_t> moves1{
+  {d3, f5, 0, 0},
+  {d3, f5, 0, 1}
+};
+
+BOOST_AUTO_TEST_SUITE(test_equal_to)
+
+constexpr std::initializer_list<move_t> moves2{
+  {d3, f5, 0, 0},
+  {d3, f5, 0, 1}
+};
+
+BOOST_DATA_TEST_CASE(test, moves1 * moves2, move1, move2) {
+  BOOST_CHECK_EQUAL(move1, move2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(test_not_equal_to)
+
+constexpr std::initializer_list<move_t> moves2{
+  {d3, f5, 1, 0},
+  {d3, e4, 0, 0},
+  {c2, f5, 0, 0}
+};
+
+BOOST_DATA_TEST_CASE(test, moves1 * moves2, move1, move2) {
+  BOOST_CHECK_NE(move1, move2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace chess
