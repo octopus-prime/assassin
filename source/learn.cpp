1eae91ef003c08369493e9470f488b0b533bec52
octopus-prime <mike.gresens@gmail.com>
2017-10-21 17:18:45 +0200

    position and score extraction for learning

--- source/learn.cpp
+++ source/learn.cpp
@@ -7,40 +7,57 @@
 
 //#define BOOST_SPIRIT_X3_DEBUG
 
+#include "node.hpp"
+#include "move.hpp"
+#include "io.hpp"
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/include/support_istream_iterator.hpp>
 #include <boost/fusion/include/std_tuple.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
 #include <iostream>
 
 namespace x3 = boost::spirit::x3;
 
+BOOST_FUSION_ADAPT_STRUCT(chess::move_t, from, to, promotion);
+
+namespace chess {
 namespace pgn {
 namespace ast {
 
 typedef std::optional<float> score_t;
-typedef std::tuple<std::string, score_t> move_t;
-typedef std::vector<move_t> game_t;
+typedef std::tuple<move_t, score_t> entry_t;
+typedef std::vector<entry_t> game_t;
+
+}
+namespace action {
+
+const auto square = [](auto& ctx)
+{
+	using boost::fusion::at_c;
+	auto& attr = x3::_attr(ctx);
+	x3::_val(ctx) = square_of(at_c<0>(attr) - 'a', at_c<1>(attr) - '1');
+};
 
 }
 namespace parser {
 
 x3::rule<struct game, ast::game_t> const game("game");
-x3::rule<struct move, ast::move_t> const move("move");
-x3::rule<struct identifier, std::string> const identifier("identifier");
-x3::rule<struct rank, char> const rank("rank");
-x3::rule<struct file, char> const file("file");
-x3::rule<struct comment, ast::score_t> const comment("comment");
+x3::rule<struct entry, ast::entry_t> const entry("entry");
+x3::rule<struct move, move_t> const move("move");
+x3::rule<struct square_, square_t> const square("square");
+x3::rule<struct promotion, move_t::promotion_t> const promotion("promotion");
+x3::rule<struct score, ast::score_t> const score("score");
 x3::rule<struct skip> const skip("skip");
 
-auto const game_def = *move;
-auto const move_def = identifier >> -comment;
-auto const identifier_def = file >> rank >> file >> rank >> -x3::char_("QRBN") >> x3::omit[-x3::char_("+#")];
-auto const rank_def = x3::char_("1-8");
-auto const file_def = x3::char_("a-h");
-auto const comment_def = x3::lit('{') >> -x3::float_ >> x3::omit[*(x3::char_ - x3::lit('}'))] >> x3::lit('}');
+auto const game_def = *entry;
+auto const entry_def = move >> -score;
+auto const move_def = square >> square >> -promotion >> x3::omit[-x3::char_("+#")];
+auto const square_def = (x3::char_("a-h") >> x3::char_("1-8")) [action::square];
+auto const promotion_def = x3::symbols<move_t::promotion_t>{{"Q", move_t::Q}, {"R", move_t::R}, {"B", move_t::B}, {"N", move_t::N}};
+auto const score_def = x3::lit('{') >> -x3::float_ >> x3::omit[*(x3::char_ - x3::lit('}'))] >> x3::lit('}');
 auto const skip_def = x3::lit('(') >> *(x3::char_ - x3::lit(')')) >> x3::lit(')');
 
-BOOST_SPIRIT_DEFINE(game, move, identifier, rank, file, comment, skip);
+BOOST_SPIRIT_DEFINE(game, entry, move, square, promotion, score, skip);
 
 }
 
@@ -61,17 +78,21 @@ void parse(std::istream& file) {
 	while (std::getline(file, line)) {
 		if (line.empty())
 			continue;
-		auto const game = pgn::parse(line);
-		std::cout << game.size() << ' ';
-//		for (auto const& move : game)
-//			std::cout << std::get<0>(move) << ' ' << std::get<1>(move).value_or(999) << ' ';
+		auto node = std::make_unique<node_t>(node_t::INITIAL_POSITION);
+		for (auto const entry : pgn::parse(line)) {
+			auto const& move = std::get<0>(entry);
+			auto const& score = std::get<1>(entry);
+			node = std::make_unique<node_t>(*node, move);
+			std::cout << move << *node << score.value_or(999) << std::endl;
+		}
 	}
-	std::cout << std::endl;
+}
+
 }
 
 int main() {
 	try {
-		parse(std::cin);
+		chess::parse(std::cin);
 	}
 	catch (std::exception const& exception) {
 		std::cerr << exception.what() << std::endl;
